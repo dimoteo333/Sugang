48a2c82f33508f8ba6e4dfb692e7745cc15a1524
dimoteo333 <dimoteo333@gmail.com>
2021-05-25 23:52:11 +0900

    database setting

--- app.py
+++ app.py
@@ -10,6 +10,32 @@ app = Flask(__name__)
 def main():
     return render_template("main.html")
 
+
+@app.route('/register', methods=['POST'])
+def register():
+    id = request.form["id"]
+    password = request.form["password"]
+    send = request.form["send"]
+
+    cur.execute("SELECT password FROM student where ID='{}'".format(id))
+    result = cur.fetchall()
+    if send == "로그인":
+        if result:
+            if password == result[0][0]:
+                return "login"
+            else:
+                return "wrong password"
+        else:
+            return "No Users Found"
+    elif send == "회원가입":
+        if result:
+            return "학번 '{}' 가 이미 존재합니다.".format(id)
+        else:
+            cur.execute("INSERT INTO student VALUES ('{}', '{}', 컴퓨터학과, 0, '{}')".format(id, id, password))
+            connect.commit()
+            return "학번 '{}' 이 회원가입 되었습니다.".format(id)
+
+
 if __name__=='__main__':
     app.run()
 
